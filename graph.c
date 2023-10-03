0a6c6c9bf79a5b08c16d67efa55351c8b44e86d7
brenn0219 <brennoaugusto2109@gmail.com>
2023-10-03 19:05:10 -0300

    feat: structuring the graph - adjacent list

---
+++ graph.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// estrutura de no da lista
+typedef struct node {
+    int vertex;
+    struct node *next;
+} node;
+
+// ponteiro para a estrutura de dados node
+typedef struct node *link; 
+
+// grafos
+typedef struct graph {
+    int vertex; 
+    int edge;
+    link *adjacent;
+} graph;
+
+// construtor do grafo
+graph initializeGraph(FILE *file) {
+    graph graph;
+    int vertex, edge;
+    fscanf(file, "%d %d", &vertex, &edge);
+
+    graph.vertex = vertex;
+    graph.edge = 0;
+    graph.adjacent = malloc(sizeof(link) * (vertex + 1) );
+
+    for(int i = 1; i < vertex + 1; i++) {
+        graph.adjacent[i] = malloc(sizeof(struct node));
+        graph.adjacent[i]->vertex = i;
+        graph.adjacent[i]->next = NULL;
+    }
+
+    return graph;
+}
+
+// instanciando um novo no
+node* newNode(int x) {
+    node *y = malloc(sizeof(struct node));
+    y->vertex = x;
+    y->next = NULL;
+
+    return y;
+}
+
+// inserindo as relacoes dos vertices - arestas
+void insertEdge(graph graph, int v, int w) {
+    node *i = graph.adjacent[v];
+       
+    while (i != NULL) {
+        if(i->vertex == w) 
+            return;
+
+        if(i->next == NULL) 
+            break;
+        else 
+            i = i->next;
+    }
+    
+    i->next = newNode(w);
+}
+
+// construindo o grafico
+void buildGraph(graph graph, FILE *file) {
+    int v, w;
+
+    while (fscanf(file, "%d %d", &v, &w) != EOF) {
+        insertEdge(graph, v, w);  
+    }
+}
+
+int main() {
+    FILE *file = fopen("graph-test-100.txt", "r");
+
+    graph graph = initializeGraph(file);
+    buildGraph(graph, file);
+
+    node *i = graph.adjacent[1];
+   
+    fclose(file);
+    return 0;
+}
