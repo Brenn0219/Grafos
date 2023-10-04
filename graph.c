0e77ae9aa2dc6027cb60ef4d67e11efef46386f8
brenn0219 <brennoaugusto2109@gmail.com>
2023-10-04 18:19:34 -0300

    feat: dfs

--- graph.c
+++ graph.c
@@ -134,6 +134,7 @@ int highestDegreeVertex(graph directedGraph, char derection, int *vertex) {
     return highestDegree;
 }
 
+// print do conjunto dos sucessores de um veritice
 void s_verttexSet(graph graph, int vertex) {
     node *i = graph.adjacent[vertex]->next;
 
@@ -149,6 +150,7 @@ void s_verttexSet(graph graph, int vertex) {
     }
 }
 
+// print do conjunto dos predecessores de um veritice
 void p_vertexSet(graph graph, int vertex) {
     int n = graph.vertex + 1;
 
@@ -171,6 +173,7 @@ void p_vertexSet(graph graph, int vertex) {
     printf("}\n");
 }
 
+// print do conjunto de uma das "direcoes"
 void printVertexSet(graph directedGraph, int vertex, char derection) {
     void (*print)(graph, int);
 
@@ -188,19 +191,125 @@ void printVertexSet(graph directedGraph, int vertex, char derection) {
     print(directedGraph, vertex);
 }
 
+// estrutura de tabela da dfs
+typedef struct talble {
+    int *descoveryTime;
+    int *endTime;
+    int *father;
+} talble;
+
+// ordenacao do conjunto de sucessores de um determinado vertice
+static void orderSet(int set[], int n) {
+    for(int i = 0; i < n-1; i++) {
+        for(int j = i + 1; j < n; j++) {
+            if(set[j] < set[i]) {
+                int temp = set[j];
+                set[j] = set[i];
+                set[i] = temp;
+            }
+        }
+    }
+}
+
+// retorna o numero de sucessores de um conjunto
+static int setSize(node *n) {
+    int counter = 0;
+    
+    for(node *i = n->next; i != NULL; i = i->next) {
+        counter++;
+    }
+
+    return counter;
+}
+
+// criando o conjunto de sucessores de um determinado vertice
+static void lexographicSet(int set[], node *node, int n) {
+    for(int i = 0; i < n; i++) {
+        set[i] = node->vertex;
+        node = node->next;
+    }
+}
+
+// busca em profundidade 
+static talble dfs(graph graph, talble talble, int vertex, int *lifetime) {
+    int n = setSize(graph.adjacent[vertex]);
+    int set[n];
+
+    lexographicSet(set, graph.adjacent[vertex]->next, n);
+    orderSet(set, n);
+
+    talble.descoveryTime[vertex] = ++*lifetime;
+    
+    for(int i = 0; i < n; i++) {
+        if(talble.descoveryTime[set[i]] == 0) {
+            talble.father[set[i]] = vertex;
+            printf("{%d, %d} - arvore\n", vertex, set[i]);
+            dfs(graph, talble, set[i], lifetime);
+        } 
+        // else {
+        //     if(talble.endTime[set[i]] == 0) {
+        //         printf("{%d, %d} - retorno\n", vertex, set[i]);
+        //     } else if (talble.descoveryTime[vertex] < talble.descoveryTime[set[i]]) {
+        //         printf("{%d, %d} - avanco\n", vertex, set[i]);
+        //     } else {
+        //         printf("{%d, %d} - cruzamento\n", vertex, set[i]);
+        //     }
+        // }
+    }
+    
+    talble.endTime[vertex] = ++*lifetime;
+
+    return talble;
+}
+
+// inicializacao da busca em profundidade
+talble depethSearch(graph graph) {
+    int n = graph.vertex + 1, lifetime = 0;
+    talble talble;
+
+    talble.descoveryTime = (int *) malloc(sizeof(int) * n);
+    talble.endTime = (int *) malloc(sizeof(int) * n);
+    talble.father = (int *) malloc(sizeof(int) * n);
+
+    for(int i = 1; i < n; i++) {
+        talble.descoveryTime[i] = 0;
+        talble.endTime[i] = 0;
+        talble.father[i] = -1;
+    }
+
+    dfs(graph, talble, 1, &lifetime);
+
+    return talble;
+}
+
+// classificacao das arestas de um vertice especifico
+void searchVertex(int vertex, graph graph, talble talble) {
+    int n = setSize(graph.adjacent[vertex]);
+    int set[n];
+
+    lexographicSet(set, graph.adjacent[vertex]->next, n);
+
+    printf("=============================================\n");
+
+    for(int i = 0; i < n; i++) {
+        if(talble.endTime[vertex] < talble.endTime[set[i]] && talble.descoveryTime[vertex] > talble.descoveryTime[set[i]]) {
+            printf("{%d, %d} - retorno\n", vertex, set[i]);
+        } else if (talble.descoveryTime[vertex] < talble.descoveryTime[set[i]]) {
+            printf("{%d, %d} - avanco\n", vertex, set[i]);
+        } else {
+            printf("{%d, %d} - cruzamento\n", vertex, set[i]);
+        }
+    }
+}
+
 int main() {
     FILE *file = fopen("graph-test-100.txt", "r");
 
     graph graph = initializeGraph(file);
     buildGraph(graph, file);
+    talble talble = depethSearch(graph);
+    searchVertex(8, graph, talble);
 
-    int vertex, degree;
-    char direction = 'p';
-
-    degree = highestDegreeVertex(graph, direction, &vertex);
-    printf("vertex: %d degree: %d\n", vertex, degree);
-    printVertexSet(graph, vertex, direction);
-   
     fclose(file);
     return 0;
 }
