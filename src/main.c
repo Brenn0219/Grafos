0f50bd405172634c36f6ce4f3b9846b142a5fd79
brenn0219 <brennoaugusto2109@gmail.com>
2023-10-09 13:40:01 -0300

    feat: main code

---
+++ src/main.c
@@ -0,0 +1,15 @@
+#include "stdlib.h"
+#include "../include/graph.h"
+#include "../include/print_graph.h"
+
+int main() {
+    graph graph;
+    table table;
+    char *path = "data/graph-test-100.txt";
+
+    build_graph(&graph, path);
+    table = dfs(graph);
+    print_edge_classification(8, graph, table);
+
+    return 0;
+}
