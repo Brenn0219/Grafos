b5fa83ccc59ee8cdaecff40dc7a05d5d3a7c2222
brenn0219 <brennoaugusto2109@gmail.com>
2023-10-18 16:28:44 -0300

    refactor: print graph library

--- include/print_graph.h
+++ include/print_graph.h
@@ -2,10 +2,10 @@
 #include "graph.h"
 
 // print do conjunto dos sucessores de um veritice
-void print_successors_set(Graph graph, int vertex);
-// print do conjunto dos predecessores de um veritice
-void print_predecessor_set(Graph graph, int vertex);
-// print do conjunto de uma das "direcoes"
-void print_vertex_set(Graph directedGraph, int vertex, char derection);
-// classificacao das arestas de um vertice especifico
-void print_edge_classification(int vertex, Graph graph, table table);
+// void print_successors_set(Graph graph, int vertex);
+// // print do conjunto dos predecessores de um veritice
+// void print_predecessor_set(Graph graph, int vertex);
+// // print do conjunto de uma das "direcoes"
+// void print_vertex_set(Graph directedGraph, int vertex, char derection);
+// // classificacao das arestas de um vertice especifico
+// void print_edge_classification(int vertex, Graph graph, table table);
--- src/print_graph.c
+++ src/print_graph.c
@@ -1,46 +1,35 @@
 #include "../include/graph.h"
+#include "../include/list.h"
+#include "../include/dfs.h"
 
 // print do conjunto dos sucessores de um veritice
-void print_successors_set(Graph graph, int vertex) {
-    Node *i = graph.adjacent[vertex]->next;
-
+void print_successors_set(Graph graph, int v) {
     printf("sucessors = {");
-    while (i != NULL) {
-        if(i->next != NULL) {
-            printf("%d - ", i->vertex);
-        } else {
-            printf("%d}\n", i->vertex);
-        }
-
-        i = i->next;
-    }
+    for(Cell *i = list_head(graph.adjacent[v]); i != NULL; i = list_next(i)) 
+        printf("%d - ", list_data(i));
+    printf("\n");
 }
 
 // print do conjunto dos predecessores de um veritice
-void print_predecessor_set(Graph graph, int vertex) {
-    int n = graph.vertex + 1;
+void print_predecessor_set(Graph graph, int v) {
+    int n = graph.vcount + 1;
 
     printf("predecessors = {");
     for(int i = 1; i < n; i++) {
-        if(i != vertex) {
-            Node *j = graph.adjacent[i]->next;
-
-            while (j != NULL) {
-                if(j->vertex == vertex) {
+        if(i != v) {
+            for(Cell *j = list_head(graph.adjacent[i]); j != NULL; j = list_next(j)) {
+                if(list_data(j) == v) {
                     printf("%d - ", i);
                     break;
                 }
-
-                j = j->next;
             }
-            
         }
     }
     printf("}\n");
 }
 
 // print do conjunto de uma das "direcoes"
-void print_vertex_set(Graph directedGraph, int vertex, char derection) {
+void print_vertex_set(Graph directedGraph, int v, char derection) {
     void (*print)(Graph, int);
 
     switch (derection) {
@@ -54,27 +43,22 @@ void print_vertex_set(Graph directedGraph, int vertex, char derection) {
             break;
     }
 
-    print(directedGraph, vertex);
+    print(directedGraph, v);
 }
 
 // classificacao das arestas de um vertice especifico
-void print_edge_classification(int vertex, Graph graph, table table) {
-    int n = set_size(graph.adjacent[vertex]);
-    int set[n];
-
-    lexographic_set(set, graph.adjacent[vertex]->next, n);
-
+void print_edge_classification(int v, Graph graph, DfsTable table) {
     printf("=============================================\n");
 
-    for(int i = 0; i < n; i++) {
-        if (vertex == table.father[set[i]]) {
-            printf("{%d, %d} - arvore\n", vertex, set[i]);
-        } else if(table.endTime[vertex] < table.endTime[set[i]] && table.descoveryTime[vertex] > table.descoveryTime[set[i]]) {
-            printf("{%d, %d} - retorno\n", vertex, set[i]);
-        } else if (table.descoveryTime[vertex] < table.descoveryTime[set[i]]) {
-            printf("{%d, %d} - avanco\n", vertex, set[i]);
+    for(Cell *i = list_head(graph.adjacent[v]); i != NULL; i = list_next(i)) {
+        if (v == table.father[list_data(i)]) {
+            printf("{%d, %d} - arvore\n", v, list_data(i));
+        } else if(table.end[v] < table.end[list_data(i)] && table.start[v] > table.start[list_data(i)]) {
+            printf("{%d, %d} - retorno\n", v, list_data(i));
+        } else if (table.start[v] < table.start[list_data(i)]) {
+            printf("{%d, %d} - avanco\n", v, list_data(i));
         } else {
-            printf("{%d, %d} - cruzamento\n", vertex, set[i]);
+            printf("{%d, %d} - cruzamento\n", v, list_data(i));
         }
     }
 }
