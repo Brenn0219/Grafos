cde471870ed0daeda93ab3e473c6d92fa0d40d30
brenn0219 <brennoaugusto2109@gmail.com>
2023-10-24 00:51:33 -0300

    refactor: print more abstract graph

--- src/print_graph.c
+++ src/print_graph.c
@@ -3,11 +3,12 @@
 #include "../include/list.h"
 
 void print_graph(Graph *graph) {
+    
     for(Cell *element = list_head(graph->adjlists); element != NULL; element = list_next(element)) {
-        printf("%d: ", *((int *)(((AdjList *) list_data(element))->vertex)));
+        printf("%d: ", ((Vertex *)(((AdjList *) list_data(element))->vertex))->vertice);
 
         for(Cell *i = list_head(((AdjList *) list_data(element))->adjacent); i != NULL; i = list_next(i))
-            printf("%d -", *((int *)(i->data)));
+            printf("%d -", ((Vertex *)(i->data))->vertice);
         printf("\n");
     }
 }
